4090b38978bab1eb16efbdeaf9d5b2da20a393c2
Michael <careyamichael2002@gmail.com>
2026-06-23 18:23:08 -0600

    test

--- EdmontonEvents.Web/Program.cs
+++ EdmontonEvents.Web/Program.cs
@@ -7,6 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
@@ -49,7 +50,6 @@ app.MapControllerRoute(
 
 app.MapControllerRoute(
     name: "Accounts",
-
     pattern: "{controller=Account}/{action=Dashboard}")
     .WithStaticAssets();
 
